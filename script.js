08d1b8f1cce97d4786c9445affebc2020ea22db8
Nguyentuan3304 <22028209@vnu.edu.vn>
2024-02-14 21:49:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ heart.addEventListener('click', () => {
 
     // Create a paragraph element
     const message = document.createElement('p');
-    message.textContent = 'a gift for Khanh_Linh !!!';
+    message.textContent = 'I promise you i will try harder to be better. I have battles with things inside me for longer than you know; i do not know what they are or why theyre here, i only know that they feel manageable, defeatable, when im around you, youre sunlight falling through trees. You are laughter that breaks through the sadness... You arent the world but you are everything that make the world good.Without you, my life would still exist, but thats all it would manage to do.   ';
     message.style.textAlign = 'center';
     message.style.marginTop = '50%';
 
